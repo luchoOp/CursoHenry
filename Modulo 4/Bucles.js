bd5c98f353500d87a340450b1a8482a81507f0be
luciano opitz <pcflamaa12@gmail.com>
2023-06-12 03:01:55 -0300

    Bucles

---
+++ Modulo 4/Bucles.js
@@ -0,0 +1,13 @@
+var suma = 0;
+for ( var i = 0; i < 10; i++){
+    suma = suma + i;
+    console.log("variable de interaccion " + suma);
+}
+console.log("variable suma " + suma);
+
+suma = 0;
+
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma); 
+}
