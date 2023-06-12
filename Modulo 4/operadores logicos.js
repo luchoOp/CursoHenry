b80585127c58a86af16c136b9707a84f1cd5ef10
luciano opitz <pcflamaa12@gmail.com>
2023-06-12 02:50:26 -0300

    Pre Curso Henry Carpeta

---
+++ Modulo 4/operadores logicos.js
@@ -0,0 +1,32 @@
+function Mayor5Menor10yPar (num){
+    if (num>5 && num<10 && num % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+Mayor5Menor10yPar(2);
+
+function OperadorOr (str){
+    if (str == "Henry" || str.length < 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+OperadorOr ("Henry");
+
+function Negacion (permiso){
+    if (permiso !== true){
+        return "tiene permiso";
+    }
+}
+negacion (false);
+
+function condicionCompleja (num){
+    if (num > 9 && num % 2 = 0 || num === 3){
+        return true;
+    }else{
+        return false;
+    }
+}
