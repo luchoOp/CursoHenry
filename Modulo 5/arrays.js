b519a04a2a6721f0d86441ec32cc8aa699fec0d3
luciano opitz <pcflamaa12@gmail.com>
2023-06-12 07:31:52 -0300

    Pre Curso M05

--- Modulo 5/arrays.js
+++ Modulo 5/arrays.js
@@ -29,3 +29,53 @@ var cumplenCondicion = numeros.every((num)=>{
     return num > 5;
 });// Every para ver si todos los elementos cumplen una condicion, respuesta en booleano
 console.log(cumplenCondicion);
+
+var palabra = "henri";
+var palabraSeparada = palabra.split("");
+palabraSeparada.pop();
+palabraSeparada.push("y");
+console.log(palabraSeparada);
+var palabraArreglada = palabraSeparada.join("");
+console.log(palabraArreglada);
+
+var numeros = [1,2,3,4];
+numeros.forEach ((num)=>{console.log(num)});
+numeros.forEach ((num)=>{
+    if(num == 3){
+        console.log(num);
+    }
+});
+
+var masUno = numeros.map ((num)=>{
+    return num + 1;
+});
+console.log(masUno);
+
+var arr = [1,3,6,9,12];
+for (var i = 0; i < arr.length; i++){
+    console.log (arr[i]);
+}
+
+function encontrarLetraP (string){
+    var letrasP = string.split("");
+    for (var i = 0; i < letrasP.length; i ++){
+        if (letrasP[i]=="p"||letrasP[i]=="P"){
+            console.log("La letra P esta en la posicion " + i);
+        }
+    }
+}
+encontrarLetraP("Manuel");
+encontrarLetraP("Pablo");
+
+var arr1 = [];
+while (arr1.length < 5){
+    arr1.push("Boom");
+}
+console.log(arr1);
+
+/*var arr2 = [];
+var n = 1;
+    while (n < 3){
+        arr2.push(Math.random());
+    } //bucle infinito, evitarlo */
+
